b717857b8de95f946bded3266d1f55f933d336af
bakso6 <127104338+bakso6@users.noreply.github.com>
2023-04-05 11:11:21 +0700

    Add files via upload

---
+++ latihanmdl1SD/src/latihanmdl1sd/Hidangan.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihanmdl1sd;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Hidangan {
+    protected String namaHidangan;
+
+    public String getNamaHidangan(){
+        return namaHidangan;
+    }
+
+    public void setNamaHidangan(String namaHidangan){
+        this.namaHidangan = namaHidangan;
+    }
+
+    public String disantap() {
+        return "Makanan Dihidangkan";
+    }
+}
---
+++ latihanmdl1SD/src/latihanmdl1sd/Latihanmdl1SD.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package latihanmdl1sd;
+import java.util.ArrayList;
+/**
+ *
+ * @author ASUS
+ */
+public class Latihanmdl1SD {
+
+  
+    public static void main(String[] args) {
+        ArrayList<Konsumsi> listKonsumsi = new ArrayList<>();
+        Konsumsi<Makanan, Minuman> breakfast = new Konsumsi<>();
+        Konsumsi<Makanan, Minuman> lunch = new Konsumsi<>();
+
+        Makanan roti = new Makanan();
+        roti.setNamaHidangan("Roti Tawar");
+        Minuman susu = new Minuman();
+        susu.setNamaHidangan("Susu sapi");
+        breakfast.setKonsumsi(roti,susu);
+        listKonsumsi.add(breakfast);
+
+        Makanan nasi = new Makanan();
+        nasi.setNamaHidangan("Nasi Putih");
+        Minuman air = new Minuman();
+        air.setNamaHidangan("Air Putih");
+        lunch.setKonsumsi(nasi, air);
+        listKonsumsi.add(lunch);
+
+        System.out.println("Menu Konsumsi");
+        for (Konsumsi<Makanan, Minuman> konsumsi: listKonsumsi){
+            Makanan makanan = konsumsi.getM();
+            Minuman minuman = konsumsi.getI();
+
+            System.out.println(makanan.disantap());
+            System.out.println(minuman.disantap());
+        }
+    }
+}
